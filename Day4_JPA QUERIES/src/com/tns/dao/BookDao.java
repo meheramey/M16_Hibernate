d860d56bb116953fdbf54f681d9c83d0e0a50609
amey <amey@LAPTOP-GHF5MGC8>
2022-06-17 15:17:09 +0530

    first commit

---
+++ Day4_JPA QUERIES/src/com/tns/dao/BookDao.java
@@ -0,0 +1,18 @@
+package com.tns.dao;
+
+import java.util.List;
+
+import com.tns.entities.Book;
+
+public interface BookDao 
+{
+	
+	public abstract List<Book> getBookByTitle(String title);
+	public abstract Long getBookCount();
+	public abstract List<Book> getAuthorBooks(String author);
+	public abstract List<Book>getAllBooks();
+	public abstract List<Book> getBooksInPriceRange(double low ,double high);
+	public abstract Book getBookById(int id);
+	
+
+}ss
